97c5ea02f636750875362358fe6a03117e39b630
Gurparvesh Singh <66719267+GurparveshGrewal@users.noreply.github.com>
2024-03-26 16:49:05 +0530

    feat: addd date time package

--- lib/features/stats/views/stats_screen.dart
+++ lib/features/stats/views/stats_screen.dart
@@ -1,5 +1,7 @@
+import 'package:expense_tracker_app/features/home/widgets/expense_card.dart';
 import 'package:expense_tracker_app/features/stats/widgets/chart_widget.dart';
 import 'package:flutter/material.dart';
+import 'package:intl/intl.dart';
 
 class StatsPage extends StatelessWidget {
   const StatsPage({super.key});
@@ -19,13 +21,53 @@ class StatsPage extends StatelessWidget {
         child: Column(
           children: [
             Container(
-              height: MediaQuery.of(context).size.width,
+              height: MediaQuery.of(context).size.width / 1.5,
               width: MediaQuery.of(context).size.width,
               decoration: BoxDecoration(
                 borderRadius: BorderRadius.circular(10),
               ),
               child: const MyChart(),
             ),
+            const SizedBox(
+              height: 10,
+            ),
+            Padding(
+              padding: const EdgeInsets.symmetric(horizontal: 8.0),
+              child: Row(
+                mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                children: [
+                  Text(
+                    DateFormat.yMMMd().format(DateTime.now()),
+                    style: TextStyle(
+                      color: Colors.grey.shade800,
+                      fontSize: 15,
+                    ),
+                  ),
+                  Text(
+                    "-\$500.00",
+                    style: TextStyle(
+                      color: Theme.of(context).colorScheme.outline,
+                      fontSize: 14,
+                    ),
+                  ),
+                ],
+              ),
+            ),
+            const SizedBox(
+              height: 10,
+            ),
+            Expanded(
+              child: ListView.builder(
+                  itemCount: 3,
+                  itemBuilder: (context, index) {
+                    return ExpenseCard(
+                      icon: Icons.food_bank_outlined,
+                      backgroundColor: index % 2 == 0
+                          ? Theme.of(context).colorScheme.secondary
+                          : Theme.of(context).colorScheme.primary,
+                    );
+                  }),
+            )
           ],
         ),
       ),
